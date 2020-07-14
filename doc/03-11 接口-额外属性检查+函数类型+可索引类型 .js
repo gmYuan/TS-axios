2634c98cd29dfe40cc9e6cf72d930f2dd6fe7dc3
yuanguangming <yuanguangming@idaddy.cn>
2020-07-14 19:35:09 +0800

    update: 去除多余文档

--- doc/03-11 接口-额外属性检查+函数类型+可索引类型 .js
+++
@@ -1,8 +0,0 @@
-var getComment;
-getComment = function (api, para) {
-    var result = api.push(para);
-    return result > 0;
-};
-var arrex2 = { 'name': 'ygm', 'age': '22' };
-console.log(arrex2);
-//# sourceMappingURL=03-11 接口-额外属性检查+函数类型+可索引类型 .js.map
--- doc/03-7 变量- 解构.ts
+++
@@ -1,7 +0,0 @@
-// 对象解构
-function showObj(obj: {name: string, age ?: number}) {
-  let {name, age = 20} = obj
-  console.log(age)
-}
-
-showObj({name: 'test'})
